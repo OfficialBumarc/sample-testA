349650967a80564be74999e93f96d630e7698552
OfficialBumarc <118039330+OfficialBumarc@users.noreply.github.com>
2022-11-12 13:39:10 +0000

    Add files via upload

---
+++ test1 upload.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print( "hello world!")
+
+
+# In[2]:
+
+
+tuple1 = (7.8, 8.2, 6.5)
+tuple2 = (6,8,5,3,2)
+new_tuple = tuple1 + tuple2
+print(new_tuple)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
